e2bcc79b16c8f7d0389d78269d65588168354e02
MyxopToB <myxoptob@gmail.com>
2018-10-16 16:33:28 +0600

    2

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace EntFr
                 Hdd disk1 = new Hdd {Manufacturer = "Seagate", Model = "sg666ujh", Capacity = 750, SerialNumber="98989775674", RegistrationNumber="027/765"};
                 db.Hdd.Add(disk1);
                 db.SaveChanges();
+                //dhdfhdfh
             }
             
             
